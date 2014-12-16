9327f2526a0c877953978fc8cf275f00c602587b
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-12-16 10:57:26 +0800

    zy: add missed file back
    tranformation in feature

--- src/Feature.js
+++ src/Feature.js
@@ -1,4 +1,15 @@
 var Feature = {
+    transformData: function(grades) {
+        var gradesObj = [];
+
+        grades.forEach(function(grade) {
+            var course = grade.split(':')[0];
+            var score = parseInt(grade.split(':')[1]);
+            gradesObj.push(new Grade(course, score))
+        });
+
+        return gradesObj;
+    },
     gradesOfCourse: function(grades, allCourses) {
         var gradesOfCourseCode = [];
         var gradesOfCourseDetail = [];
