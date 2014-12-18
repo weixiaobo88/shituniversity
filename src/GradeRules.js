d7c844dcbd43349c0a91956ed06f356edaa92c84
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-12-18 16:36:01 +0800

    zy: change graderules from object to function

--- src/GradeRules.js
+++ src/GradeRules.js
@@ -1,77 +1,77 @@
-var GradeRules = {
-    getMyGradesOfCourse: function(grades, allCourses) {
-        var gradesOfCourseCode = [];
-        var myCourses = [];
+function GradeRules() {
+}
 
-        grades.forEach(function (grade) {
-            allCourses.forEach(function (course) {
-                if (course.code === grade.course) {
-                    gradesOfCourseCode.push(grade);
-                    var newCourse = course;
-                    newCourse.score = grade.score;
-                    myCourses.push(newCourse);
-                }
-            })
-        });
+GradeRules.prototype.getMyGradesOfCourse = function (grades, allCourses) {
+    var gradesOfCourseCode = [];
+    var myCourses = [];
 
-        return {
-            gradesOfCourseCode: gradesOfCourseCode,
-            myCourses: myCourses
-        }
-    },
-    getMyGradesOfPractices: function(grades, allPractices) {
-        var myPracticesScore = [];
-        var myPractices = [];
-
-        grades.forEach(function (grade) {
-            allPractices.forEach(function (practice) {
-                if (practice.name === grade.course) {
-                    myPracticesScore.push(grade);
-                    var newCourse = practice;
-                    newCourse.score = grade.score;
-                    myPractices.push(newCourse);
-                }
-            })
-        });
-
-        return {
-            myPracticesScore: myPracticesScore,
-            myPractices: myPractices
-        }
-    },
-    qualifiedGrades: function(grades) {
-        var qualifiedGrades = [];
-        grades.forEach(function(grade) {
-            if(grade.score > 60) {
-                qualifiedGrades.push(grade);
+    grades.forEach(function (grade) {
+        allCourses.forEach(function (course) {
+            if (course.code === grade.course) {
+                gradesOfCourseCode.push(grade);
+                var newCourse = course;
+                newCourse.score = grade.score;
+                myCourses.push(newCourse);
             }
-        });
+        })
+    });
 
-        return qualifiedGrades;
-    },
-    removeUnreplaceablePractices: function(gradesWithPracticeDetail) {
-        var gradesWithPracticeDetailOfAllReplaceable = gradesWithPracticeDetail;
-
-        gradesWithPracticeDetailOfAllReplaceable.forEach(function(grade, index) {
-            if(!grade.replaceableCourses) {
-                gradesWithPracticeDetailOfAllReplaceable.splice(index, 1);
-            }
-        });
-
-        return gradesWithPracticeDetailOfAllReplaceable;
-    },
-    calculateCredits: function (courses) {
-        var credits = {
-            compulsory: 0,
-            elective: 0
-        };
-
-        courses.forEach(function (course) {
-            credits.compulsory += (course.type === CourseType.COMPULSORY ? course.credit : 0);
-            credits.elective += (course.type === CourseType.ELECTIVE ? course.credit : 0);
-        });
-
-        return credits;
+    return {
+        gradesOfCourseCode: gradesOfCourseCode,
+        myCourses: myCourses
     }
+};
+
+GradeRules.prototype.getMyGradesOfPractices = function (grades, allPractices) {
+    var myPractices = [];
+
+    grades.forEach(function (grade) {
+        allPractices.forEach(function (practice) {
+            if (practice.name === grade.course) {
+                var newCourse = Object.create(practice);
+                newCourse.score = grade.score;
+                myPractices.push(newCourse);
+            }
+        })
+    });
+
+    return myPractices;
+};
+
+GradeRules.prototype.qualifiedGrades = function (grades) {
+    var qualifiedGrades = [];
+    grades.forEach(function (grade) {
+        if (grade.score > 60) {
+            qualifiedGrades.push(grade);
+        }
+    });
+
+    return qualifiedGrades;
+};
+
+GradeRules.prototype.removeUnreplaceablePractices = function (gradesWithPracticeDetail) {
+    var gradesWithPracticeDetailOfAllReplaceable = gradesWithPracticeDetail;
+
+    gradesWithPracticeDetailOfAllReplaceable.forEach(function (grade, index) {
+        if (!grade.replaceableCourses) {
+            gradesWithPracticeDetailOfAllReplaceable.splice(index, 1);
+        }
+    });
+
+    return gradesWithPracticeDetailOfAllReplaceable;
+};
+
+GradeRules.prototype.calculateCredits = function (courses) {
+    var credits = {
+        compulsory: 0,
+        elective: 0
+    };
+
+    courses.forEach(function (course) {
+        credits.compulsory += (course.type === CourseType.COMPULSORY ? course.credit : 0);
+        credits.elective += (course.type === CourseType.ELECTIVE ? course.credit : 0);
+    });
+
+    return credits;
 };
 
